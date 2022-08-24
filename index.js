f77941f8340864369e45eac39eb1f1d51bf06375
coding-guy <1codingguy@gmail.com>
2022-08-24 23:38:37 +0100

    setup connection to DB

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+require('dotenv').config()
+
+const connectDB = require('./db/connect')
+
+
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.use(cors());
+app.use(cors())
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use('/public', express.static(`${process.cwd()}/public`))
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.post('/api/shorturl', (req, res) => {
+  const { url: original_url } = req.body
+
+  // To-do: validate the URL
+
+  res.status(201).json(req.body)
+})
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
